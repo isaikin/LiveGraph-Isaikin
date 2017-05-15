9566319b23a5078b41c024faffda3fb2206beacd
DESKTOP-L83JPJ8\isaik <isaikin.v@gmail.com>
2017-05-15 15:21:09 -0700

    Added test

--- LiveGraph/LiveGraph.UI/Controllers/AppTestsController.cs
+++ LiveGraph/LiveGraph.UI/Controllers/AppTestsController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LiveGraph.Common;
 
 namespace LiveGraph.UI.Controllers
 {
-    public class AppTestsController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
+	public class AppTestsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpGet]
 		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+
+		public IActionResult Create(AppTest test)
 		{
 			return View();
 		}
